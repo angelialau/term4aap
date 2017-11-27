bce48bd021a2691ccefe371dcf83667f27cc484f
angelialau <angelialkm@gmail.com>
2017-11-27 19:35:29 +0800

    implemented dark theme

--- app/src/main/java/com/example/angelia/term4androidappproject/SettingsFragment.java
+++ app/src/main/java/com/example/angelia/term4androidappproject/SettingsFragment.java
@@ -1,8 +1,11 @@
 package com.example.angelia.term4androidappproject;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+import android.support.v4.content.IntentCompat;
 
 /**
  * Created by Angelia on 20/11/17.
@@ -15,5 +18,6 @@ public class SettingsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
     }
+
 
 }
