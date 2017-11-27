0761d064c729c4d909d2c9fa385a2486f7ffe865
nosyarlin <rayson.ljk@gmail.com>
2017-11-27 22:28:26 +0800

    EditItineraryActivity reads data from intent

--- app/src/main/java/com/example/angelia/term4androidappproject/EditItineraryActivity.java
+++ app/src/main/java/com/example/angelia/term4androidappproject/EditItineraryActivity.java
@@ -3,25 +3,59 @@ package com.example.angelia.term4androidappproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.angelia.term4androidappproject.Adapters.EditItineraryAdapter;
+
+import java.util.ArrayList;
 
 public class EditItineraryActivity extends AppCompatActivity {
 
     Button buttonDoneItinerary;
+    Button buttonAddMoreLocation;
+
+    RecyclerView recyclerShowLocations;
+
+    String dateFromIntent;
+    ArrayList<String> locationsFromIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_itinerary);
 
-        buttonDoneItinerary = findViewById(R.id.buttonDoneWithItinerary);
+        // Getting data from extras
+        Intent incoming = getIntent();
+        dateFromIntent = incoming.getStringExtra(MainActivity.DATE_KEY);
+        locationsFromIntent = incoming.getStringArrayListExtra(MainActivity.LOCATION_KEY);
 
+        // Wiring up all my views
+        buttonDoneItinerary = findViewById(R.id.buttonDoneWithItinerary);
+        buttonAddMoreLocation = findViewById(R.id.buttonAddMoreLocation);
+        recyclerShowLocations = findViewById(R.id.recyclerShowLocations);
+
+        // Setting up recycler view
+        if (!dateFromIntent.isEmpty() || !locationsFromIntent.isEmpty()){
+            EditItineraryAdapter editItineraryAdapter = new EditItineraryAdapter(locationsFromIntent);
+            recyclerShowLocations.setAdapter(editItineraryAdapter);
+        }
+
+        // Setting on click listeners
         buttonDoneItinerary.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(),MainActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+        buttonAddMoreLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),NewItineraryActivity.class);
                 startActivity(intent);
             }
         });
